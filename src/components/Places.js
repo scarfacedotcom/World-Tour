1169fd39427f3ffc66054dbb958164e5dcb393f3
scarfacedotcom <peteradaaku@gmail.com>
2022-06-17 09:45:24 +0100

    addedd the loading component

--- src/components/Places.js
+++ src/components/Places.js
@@ -1,18 +1,31 @@
 import { useEffect, useState } from "react";
 import Place from "./Place";
+import Loading from "./Loading";
 
 const Places = () => {
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true)
   
   function fetchTours() {
+    setLoading(true)
     fetch("https://course-api.com/react-tours-project")
       .then((res) => res.json())
+      setLoading(false)
       .then((data) => setData(data));
+      
   }
 
   useEffect(() => {
     fetchTours();
   }, []);
+
+  if(loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
 
   const filteredTours = (id) => {
     const newTours = data.filter((place) => place.id !== id)
